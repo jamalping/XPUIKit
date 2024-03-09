75ce06f191db87b1624eeb9235e2be9686d362fb
jamalping <420436097@qq.com>
2024-03-09 20:02:11 +0800

    frist commit

---
+++ Example/Pods/Target Support Files/Pods-XPUIKit_Tests/Pods-XPUIKit_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_XPUIKit_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_XPUIKit_Tests
+@end
---
+++ Example/Pods/Target Support Files/XPUIKit/XPUIKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_XPUIKit : NSObject
+@end
+@implementation PodsDummy_XPUIKit
+@end
